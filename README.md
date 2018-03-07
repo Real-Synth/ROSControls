ef7442aa991d8f4b4f90005be42206aacdc088ea
Richard Schubert <rs@directx9.de>
2018-03-07 14:17:26 +0100

    Added stub

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# ROSControls
-Support for ROS controlled vehicles in Unreal Engine Projects
+# ROSControls for Unreal Engine 4
+
+This plugin enables control of wheeled vehicles via ROS messages.
+
+In order to use this plugin you also need to add the [ROSIntegration Core Plugin](https://github.com/code-iai/ROSIntegration)  to your project.
+
+## Usage
+After installing this plugin and the core ROSIntegration plugin, you can load your UE4 project. If the plugin is not enabled in the project yet, you can do this in the UE4Editor in Edit -> Plugins.
+When the plugin is loaded correctly, you should be able to select the AWheeledVehicleController as the Player Controller of the GameMode Settings of your level.
---
+++ Source/ROSControls/Public/WheeledVehicleController.h
@@ -0,0 +1,33 @@
+// Copyright 2018 RealSynth GmbH
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "WheeledVehicleController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROSCONTROLS_API AWheeledVehicleController : public APlayerController
+{
+	GENERATED_BODY()
+	
+  /// @}
+  // ===========================================================================
+  /// @name APlayerController overrides
+  // ===========================================================================
+  /// @{
+public:
+
+  virtual void Possess(APawn *aPawn) override;
+
+  void Tick(const float DeltaTime);
+	
+private:
+
+    /** vehicle simulation component */
+    UPROPERTY(Category = Vehicle, VisibleDefaultsOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+    class UWheeledVehicleMovementComponent* VehicleMovement;
+};
